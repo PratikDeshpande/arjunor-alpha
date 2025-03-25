e086b8d42b97e336f68ae6c4110d7068b39a52c4
pdeshpande1 <pdeshpande1@U-1QLPLQXAXR9I.corp.arjunor.com>
2025-03-25 17:51:38 -0400

    implement basic get and set functionality

--- src/eval.cpp
+++ src/eval.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <memory>
 #include <ctime>
+#include "resp.h"
 
 namespace eval {
     void eval_ping(std::vector<std::string> arguments, int new_fd, std::shared_ptr<store::ObjectStore> object_store) {
@@ -14,29 +15,15 @@ namespace eval {
         }
 
         if (arguments.size() == 1) {
-            // TODO: Use encode function to convert to RESP format
-            std::ostringstream response_stream;
-            response_stream << "$" << arguments[0].size() << "\r\n" << arguments[0] << "\r\n";
-            std::string response = response_stream.str();
-            auto char_string = response.c_str();
-            auto byte_string = (std::byte*)char_string;
-            printf("response size: %li\n", response.size());
-            auto bytes_sent = send(new_fd, byte_string, response.size(), 0);
-
-            printf("bytes sent: %li\n", bytes_sent);
+            std::vector<std::byte> response_byte_vector = resp::encode_bulk_string(arguments[0]);
+            auto bytes_sent = send(new_fd, response_byte_vector.data(), response_byte_vector.size(), 0);
             if (bytes_sent <= 0) {
                 std::cout << "Error sending bytes" << std::endl;
                 throw std::invalid_argument("Error sending bytes");
             }
         } else {
-            // TODO: Use encode function to convert to RESP format
-            std::string response = "+PONG\r\n";
-            auto char_string = response.c_str();
-            auto byte_string = (std::byte*)char_string;
-            printf("response size: %li\n", response.size());
-            auto bytes_sent = send(new_fd, byte_string, response.size(), 0);
-
-            printf("bytes sent: %li\n", bytes_sent);
+            std::vector<std::byte> response_byte_vector = resp::encode_simple_string("PONG");
+            auto bytes_sent = send(new_fd, response_byte_vector.data(), response_byte_vector.size(), 0);
             if (bytes_sent <= 0) {
                 std::cout << "Error sending bytes" << std::endl;
                 throw std::invalid_argument("Error sending bytes");
@@ -50,7 +37,24 @@ namespace eval {
             std::cout << "Error: Wrong number of arguments" << std::endl;
             throw std::invalid_argument("ERR wrong number of arguments for 'set' command");
         } else {
-            throw std::invalid_argument("Not implemented");
+            
+            auto key = arguments[0];
+            auto value = arguments[1];
+            auto object = std::make_shared<store::Object>();
+
+            for (auto strChar: value) {
+                object->data.push_back(std::byte(strChar));
+            }
+            object->timestamp = std::time(nullptr);
+            object_store->insert(key, object);
+            
+            std::vector<std::byte> response_byte_vector = resp::encode_simple_string("OK");
+            auto bytes_sent = send(new_fd, response_byte_vector.data(), response_byte_vector.size(), 0);
+            if (bytes_sent <= 0) {
+                std::cout << "Error sending bytes" << std::endl;
+                throw std::invalid_argument("Error sending bytes");
+            }
+                
         }
     }
 
@@ -59,7 +63,29 @@ namespace eval {
             std::cout << "Error: Wrong number of arguments" << std::endl;
             throw std::invalid_argument("ERR wrong number of arguments for 'get' command");
         } else {
-            throw std::invalid_argument("Not implemented");
+            auto key = arguments[0];
+            auto object = object_store->get(key);
+            if (object != nullptr) {
+                auto object_data = object->data;
+                std::string data_string(  reinterpret_cast<const char*>(object_data.data()), reinterpret_cast<const char*>(object_data.data()  + object_data.size() ));
+
+                // TODO: should all values be returned as bulk strings?
+                std::vector<std::byte> response_byte_vector = resp::encode_bulk_string(data_string);
+                auto bytes_sent = send(new_fd, response_byte_vector.data(), response_byte_vector.size(), 0);
+                if (bytes_sent <= 0) {
+                    std::cout << "Error sending bytes" << std::endl;
+                    throw std::invalid_argument("Error sending bytes");
+                }
+                    
+
+            } else {
+                std::vector<std::byte> response_byte_vector = resp::encode_null();
+                auto bytes_sent = send(new_fd, response_byte_vector.data(), response_byte_vector.size(), 0);
+                if (bytes_sent <= 0) {
+                    std::cout << "Error sending bytes" << std::endl;
+                    throw std::invalid_argument("Error sending bytes");
+                }
+            }
         }
     }
 
